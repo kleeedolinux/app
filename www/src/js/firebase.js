5b14b3448892566ddf49b446cfffe746f9d46adf
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2025-01-13 14:56:25 -0300

    update www/src/js/firebase.js

--- www/src/js/firebase.js
+++ www/src/js/firebase.js
@@ -1,1 +1,14 @@
-console.log('Firebase Analytics Initialized:', analytics);
+  // Inicializar o Firebase
+  const app = initializeApp({
+    apiKey: "AIzaSyBLkMv2N_cnG6q-9spUj01wvxRnfWSr6XY",
+    authDomain: "cookie-brasil.firebaseapp.com",
+    projectId: "cookie-brasil",
+    storageBucket: "cookie-brasil.appspot.com",
+    messagingSenderId: "202105498916",
+    appId: "1:202105498916:web:d161e68c42e66c8f8997b1",
+    measurementId: "G-6KZE06S4SF"
+  });
+
+  // Ativar Analytics se necessário
+  const analytics = getAnalytics(app);
+  console.log(analytics)
