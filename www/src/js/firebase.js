50ad01081b74e0ace5f1a4e50ed247f98f1458fa
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2025-01-13 15:08:07 -0300

    update www/src/js/firebase.js

--- www/src/js/firebase.js
+++ www/src/js/firebase.js
@@ -1,10 +1,10 @@
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBLkMv2N_cnG6q-9spUj01wvxRnfWSr6XY",
-    authDomain: "cookie-brasil.firebaseapp.com",
-    projectId: "cookie-brasil",
-    storageBucket: "cookie-brasil.appspot.com",
-    messagingSenderId: "202105498916",
-    appId: "1:202105498916:web:d161e68c42e66c8f8997b1",
-    measurementId: "G-6KZE06S4SF"
+  apiKey: "AIzaSyBLkMv2N_cnG6q-9spUj01wvxRnfWSr6XY",
+  authDomain: "cookie-brasil.firebaseapp.com",
+  projectId: "cookie-brasil",
+  storageBucket: "cookie-brasil.appspot.com",
+  messagingSenderId: "202105498916",
+  appId: "1:202105498916:web:d161e68c42e66c8f8997b1",
+  measurementId: "G-6KZE06S4SF"
   });
 console.log(app);
